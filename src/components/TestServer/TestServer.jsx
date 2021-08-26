3949a6ea985092dd597dafc45734baf4b380a1f5
Waksel <axeloliovier59@gmail.com>
2021-08-26 17:46:43 +0200

    save

--- src/components/TestServer/TestServer.jsx
+++ src/components/TestServer/TestServer.jsx
@@ -3,14 +3,24 @@ import TestServerController from './TestServerController';
 
 const TestServer = () => {
   const [value, setValue] = useState();
+  const [number, setNumber] = useState(0);
   useEffect(() => {
+    setNumber(8);
     TestServerController.load().then((response) => {
       setValue(response);
     });
-  }, []);
+  },
+  []);
+  const handle = (ev)=>{
+    setNumber(ev.currentTarget.value)
+  }
   return (
     <div>
       <h1>Test</h1>
+      <form>
+       <input type="text" value={number} onChange={handle}/>
+       <button name="InputValue"> </button>
+      </form>
       {value}
     </div>
   );
--- src/components/TestServer/TestServerController.js
+++ src/components/TestServer/TestServerController.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const TestServerController = {
   load: () => new Promise((success) => {
     const body = { username: 'tetet', password: 'zererere' };
-    axios.post('http://localhost:8080/authentification/', body).then((response) => {
+    axios.get('http://localhost:8080/testroute/test/22/', body).then((response) => {
       success(response.data);
     });
   }),
